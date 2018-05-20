a09c19aab524855709571c9b8dc4b969a507b888
Administrator <Administrator@Z258H7FRH70R1WT>
2018-05-20 13:15:16 +0800

    の232132132143124212

--- src/net/aizhuoyue/demo/demo.java
+++ src/net/aizhuoyue/demo/demo.java
@@ -3,6 +3,11 @@ package net.aizhuoyue.demo;
 public class demo {
       public static void main(String[] args) {
 		System.out.println("小学生");
+		System.out.println("小学生");
+		System.out.println("小学生");
+		System.out.println("小学生");
+		System.out.println("小学生");
+		System.out.println("小学生");
 		System.out.println();
 		System.out.println();
 	}
